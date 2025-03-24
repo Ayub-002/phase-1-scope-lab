f3c9107ff944975bd811c83d69244f0923037648
Ayub-002 <ayub.ndungu@student.moringaschool.com>
2025-03-24 14:00:30 +0300

    adds scope

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicitly creates a global variable
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'someone';
+
+// Function to change leastFavoriteCustomer (should fail)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw an error
+}
